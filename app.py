1b0024d65380f8fb4fc555a06295dc4976e447ae
Daniel001-odii <xenithheight@gmail.com>
2023-08-24 22:17:06 +0100

    test ytubefetch removal

--- app.py
+++ app.py
@@ -90,47 +90,6 @@ def api_submit():
 
 
 
-# API endpoint to fetch video information
-@app.route("ytubefetch/api/video_info", methods=["GET"])
-def api_video_info():
-    url = request.args.get("url")
-    if url:
-        try:
-            yt = YouTube(url)
-            video_streams = yt.streams.filter(file_extension="mp4", progressive=True)
-            thumbnail_url = yt.thumbnail_url
-            resolutions = [{"resolution": stream.resolution, "size_mb": stream.filesize / (1024 * 1024), "download_link": f"/api/download?url={url}&resolution={stream.resolution}"} for stream in video_streams]
-            # resolutions = [{"resolution": stream.resolution, "size": stream.filesize, "download_link": f"/api/download?url={url}&resolution={stream.resolution}"} for stream in video_streams]
-            return jsonify({"title": yt.title, "resolutions": resolutions, "thumbnail_url": thumbnail_url})
-        except Exception as e:
-            return jsonify({"error": "An error occurred: " + str(e)})
-    return jsonify({"error": "URL parameter is required."})
-
-    
-# API endpoint to download a video
-@app.route("ytubefetch/api/download", methods=["GET"])
-def api_download():
-    url = request.args.get("url")
-    resolution = request.args.get("resolution")
-    if not url or not resolution:
-        return jsonify({"error": "URL and resolution are required parameters."})
-    try:
-        yt = YouTube(url)
-        video_stream = yt.streams.filter(res=resolution, file_extension="mp4", progressive=True).first()
-        video_file = video_stream.download()
-        filename = f"{yt.title}_{resolution}.mp4"
-        with open(video_file, 'rb') as file:
-            video_content = file.read()
-        response = make_response(video_content)
-        response.headers['Content-Type'] = 'video/mp4'
-        response.headers['Content-Disposition'] = f'attachment; filename={filename}'
-        return response
-
-    except Exception as e:
-        return jsonify({"error": "An error occurred while downloading: " + str(e)})
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
